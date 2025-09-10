ee43f1c78bcf81debd7201844f4e2b064a82d8c8
Colbee Thrasher <colbeethr@gmail.com>
2025-09-10 17:30:06 -0500

    completed operations exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace OperatorExercise
 {
+   
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int add = a + b;
+            int  sub = a - b;
+            int  mul = a * b;
+            int  quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            
+            
+            Console.WriteLine("what is the radius of the circle");
+            double radius = double.Parse(Console.ReadLine());
+            double areaOfCircle = AreaOfCircle(radius);
+            Console.WriteLine($"the area {areaOfCircle}");
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+        public static double AreaOfCircle(double radius)
+        { 
+            return Math.PI * radius * radius;   
         }
     }
 }
